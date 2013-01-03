ccc81bdc83dc96d1ebf16eba0d099489dbfe5f4b
Juan <j.mercedes@vissualmedia.com>
2013-01-03 16:41:21 -0400

    Vainilla installation refinerycms

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CmboBlue::Application.config.secret_token = '709c155a7e8f02d2dfb096fd02d7ec634f0e9db0dbcab6cefda242bdda93073386f187688c496acdc09be7cbe474a01d170269a9d8df4832a311cbb04220ff91'
