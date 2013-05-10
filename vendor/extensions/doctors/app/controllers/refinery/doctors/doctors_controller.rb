d508b212ca78669bfdbbc047f241cd73184a016d
Juan Mercedes <j.mercedes@thedesignpill.com>
2013-05-09 23:42:34 -0400

    fix show results

--- vendor/extensions/doctors/app/controllers/refinery/doctors/doctors_controller.rb
+++ vendor/extensions/doctors/app/controllers/refinery/doctors/doctors_controller.rb
@@ -13,6 +13,7 @@ module Refinery
       end
 
       def show
+        @doctor = Doctor.find(params[:id])
 
         # you can use meta fields from your model instead (e.g. browser_title)
         # by swapping @page for @doctor in the line below:
